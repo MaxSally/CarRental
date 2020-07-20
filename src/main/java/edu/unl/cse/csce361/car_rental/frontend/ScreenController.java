12de8c46f92619c296e29e5555cd169990631b4a
Chloe Galinsky and Quan Nguyen <cgalinsky2qnguyen16@dev.null>
2020-07-20 16:29:20 -0700

    Created an abstract class for switchScreen method to not violate DRY

--- src/main/java/edu/unl/cse/csce361/car_rental/frontend/ScreenController.java
+++ src/main/java/edu/unl/cse/csce361/car_rental/frontend/ScreenController.java
@@ -1,4 +1,26 @@
 package edu.unl.cse.csce361.car_rental.frontend;
 
-public interface ScreenController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public abstract class ScreenController {
+
+    public void switchScreen(ActionEvent event, String screenName) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(screenName));
+        Parent parent = loader.load();
+
+        Scene scene = new Scene(parent);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(scene);
+        window.show();
+    }
+
 }
