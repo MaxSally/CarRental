5b7fe7294ce7288b70e741c9f818646b5e10c4a3
Chloe Galinsky and Quan Nguyen <cgalinsky2qnguyen16@dev.null>
2020-07-21 10:36:42 -0500

    add fx:id to data field to solve NullPointException

--- src/main/java/edu/unl/cse/csce361/car_rental/frontend/IndividualCustomerAccountController.java
+++ src/main/java/edu/unl/cse/csce361/car_rental/frontend/IndividualCustomerAccountController.java
@@ -1,13 +1,11 @@
 package edu.unl.cse.csce361.car_rental.frontend;
 
 import edu.unl.cse.csce361.car_rental.rental_logic.DataLogic;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 
 public class IndividualCustomerAccountController extends ScreenController {
     @FXML TextField txtFieldName;
@@ -20,12 +18,12 @@ public class IndividualCustomerAccountController extends ScreenController {
     @FXML TextField txtFieldCVV;
     @FXML DatePicker datePickerExpDate;
 
-
-
-    public void createIndividualCustomer(ActionEvent event) throws IOException {
-        DataLogic.getInstance().createAccount(txtFieldName.getText(), txtFieldStreetAddress1.getText(), txtFieldStreetAddress2.getText(),
-        txtFieldCity.getText(), txtFieldState.getText(), txtFieldZip.getText(), txtFieldCardNumber.getText(),
-                datePickerExpDate.getValue().getMonthValue(), datePickerExpDate.getValue().getYear(), txtFieldCVV.getText());
-        switchScreen(event, "home.fxml");
+    public void createIndividualCustomer(javafx.event.ActionEvent event) throws IOException {
+        if(DataLogic.getInstance().createIndividualCustomerAccount(txtFieldName.getText(), txtFieldStreetAddress1.getText(),
+                txtFieldStreetAddress2.getText(), txtFieldCity.getText(), txtFieldState.getText(), txtFieldZip.getText())){
+            switchScreen(event, "home.fxml");
+        }else{
+            txtFieldName.setText("Failed");
+        }
     }
 }
--- src/main/java/edu/unl/cse/csce361/car_rental/rental_logic/DataLogic.java
+++ src/main/java/edu/unl/cse/csce361/car_rental/rental_logic/DataLogic.java
@@ -1,6 +1,7 @@
 package edu.unl.cse.csce361.car_rental.rental_logic;
 
 import edu.unl.cse.csce361.car_rental.backend.Backend;
+import edu.unl.cse.csce361.car_rental.backend.Customer;
 
 public class DataLogic {
     private static DataLogic instance;
@@ -24,10 +25,9 @@ public class DataLogic {
         }
     }
 
-    public void createAccount(String name, String streetAddress1, String streetAddress2,
-                              String city, String state, String zipCode,
-                              String paymentCardNumber, int paymentCardExpirationMonth,
-                              int paymentCardExpirationYear, String paymentCardCvv){
-        Backend.getInstance().createIndividualCustomer(name, streetAddress1, streetAddress2, city, state, zipCode, paymentCardNumber, paymentCardExpirationMonth, paymentCardExpirationYear, paymentCardCvv);
+    public boolean createIndividualCustomerAccount(String name, String streetAddress1, String streetAddress2,
+                                                   String city, String state, String zipCode){
+        Customer customer = Backend.getInstance().createIndividualCustomer(name, streetAddress1, streetAddress2, city, state, zipCode);
+        return (customer != null);
     }
 }
