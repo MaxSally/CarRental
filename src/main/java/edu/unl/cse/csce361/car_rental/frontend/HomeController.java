504572cb5bc5ece5fb1087fa81e6b54eb04dd1a6
Chloe Galinsky and Quan Nguyen <cgalinsky2qnguyen16@dev.null>
2020-07-22 10:07:36 -0700

    Added an alert box to notify user of invalid login attempt

--- src/main/java/edu/unl/cse/csce361/car_rental/frontend/HomeController.java
+++ src/main/java/edu/unl/cse/csce361/car_rental/frontend/HomeController.java
@@ -2,7 +2,14 @@ package edu.unl.cse.csce361.car_rental.frontend;
 
 import edu.unl.cse.csce361.car_rental.rental_logic.DataLogic;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -21,11 +28,36 @@ public class HomeController extends ScreenController{
         switchScreen(event, "managerOptions.fxml");
     }
 
+    public void invalidLoginAlert() {
+
+        Stage window = new Stage();
+        //application modality allows the application and screen below to remain open,
+        //but non-functional until the alert screen closes
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle("Login Failed");
+        window.setMinHeight(150);
+        window.setMinWidth(300);
+
+        Label label = new Label();
+        label.setText("Sorry, it seems you entered an invalid login!");
+
+        Button tryAgain = new Button("Try Again");
+        tryAgain.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, tryAgain);
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
     public void logIn(javafx.event.ActionEvent event) throws IOException {
-        if(DataLogic.getInstance().logIn(txtUsername.getText())){
+        if(DataLogic.getInstance().logIn(txtUsername.getText())) {
             switchScreen(event, "filters.fxml");
-        }else{
-            txtUsername.setText("Failed");
+        } else {
+            invalidLoginAlert();
         }
     }
 }
