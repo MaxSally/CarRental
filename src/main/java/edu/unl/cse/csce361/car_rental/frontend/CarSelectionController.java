f0e23d097fe70bce7b5a3b1b637e6a5b3d6c4b03
Chloe Galinsky and Quan Nguyen <cgalinsky2qnguyen16@dev.null>
2020-07-23 12:02:51 -0700

    Initialized options for sort by price choice box

--- src/main/java/edu/unl/cse/csce361/car_rental/frontend/CarSelectionController.java
+++ src/main/java/edu/unl/cse/csce361/car_rental/frontend/CarSelectionController.java
@@ -4,6 +4,7 @@ import edu.unl.cse.csce361.car_rental.rental_logic.DataLogic;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 
 import java.io.IOException;
@@ -12,14 +13,19 @@ public class CarSelectionController extends ScreenController {
     @FXML
     ListView listViewCar;
 
+    @FXML
+    private ChoiceBox<String> sortByPriceOptions;
+
     public void switchToFilterScreen(javafx.event.ActionEvent event) throws IOException {
         DataLogic.getInstance().resetCriteriaFilter();
         switchScreen(event, "filters.fxml");
-
     }
+    
     @FXML
     public void initialize(){
         ObservableList<String> availableCar = FXCollections.observableArrayList(DataLogic.getInstance().getValidCarDescription());
+        ObservableList<String> priceSortingChoices = FXCollections.observableArrayList("","High to Low", "Low to High");
         listViewCar.getItems().addAll(availableCar);
+        sortByPriceOptions.setItems(priceSortingChoices);
     }
 }
