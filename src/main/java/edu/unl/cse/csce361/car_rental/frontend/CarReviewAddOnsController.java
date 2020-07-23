408c16c72517f235229aecbb68c5fd13e614549b
Chloe Galinsky and Quan Nguyen <cgalinsky2qnguyen16@dev.null>
2020-07-23 12:45:33 -0700

    Created controller for the car review/add-on screen

--- src/main/java/edu/unl/cse/csce361/car_rental/frontend/CarReviewAddOnsController.java
+++ src/main/java/edu/unl/cse/csce361/car_rental/frontend/CarReviewAddOnsController.java
@@ -1,4 +1,12 @@
 package edu.unl.cse.csce361.car_rental.frontend;
 
-public class CarReviewAddOns {
+import edu.unl.cse.csce361.car_rental.rental_logic.DataLogic;
+import java.io.IOException;
+
+public class CarReviewAddOnsController extends ScreenController {
+
+    public void switchToFilterScreen(javafx.event.ActionEvent event) throws IOException {
+        DataLogic.getInstance().resetCriteriaFilter();
+        switchScreen(event, "filters.fxml");
+    }
 }
