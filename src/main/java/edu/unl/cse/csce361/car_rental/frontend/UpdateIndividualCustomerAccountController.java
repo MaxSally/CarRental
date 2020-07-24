ed17b006529516f07f933ebe64f1668d35c30842
Chloe Galinsky and Quan Nguyen <cgalinsky2qnguyen16@dev.null>
2020-07-23 19:52:23 -0700

    Added payments card information fields

--- src/main/java/edu/unl/cse/csce361/car_rental/frontend/UpdateIndividualCustomerAccountController.java
+++ src/main/java/edu/unl/cse/csce361/car_rental/frontend/UpdateIndividualCustomerAccountController.java
@@ -1,7 +1,10 @@
 package edu.unl.cse.csce361.car_rental.frontend;
 
 import edu.unl.cse.csce361.car_rental.rental_logic.DataLogic;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
@@ -20,7 +23,23 @@ public class UpdateIndividualCustomerAccountController extends ScreenController 
     TextField txtFieldState;
     @FXML
     TextField txtFieldZip;
+    @FXML
+    TextField txtFieldCardNumber;
+    @FXML
+    TextField txtFieldCVV;
+    @FXML
+    private ChoiceBox<Integer> expirationMonthOptions;
+    @FXML
+    private ChoiceBox<Integer> expirationYearOptions;
 
+    ObservableList<Integer> expirationMonthChoices = FXCollections.observableArrayList(null,1,2,3,4,5,6,7,8,9,10,11,12);
+    ObservableList<Integer> expirationYearChoices = FXCollections.observableArrayList(null,2020,2021,2022,2023,2024,2025);
+
+    @FXML
+    private void initialize() {
+        expirationMonthOptions.setItems(expirationMonthChoices);
+        expirationYearOptions.setItems(expirationYearChoices);
+    }
 
 
     public void updateIndividualCustomer(javafx.event.ActionEvent event) throws IOException {
@@ -30,8 +49,9 @@ public class UpdateIndividualCustomerAccountController extends ScreenController 
         } else {
             if(!DataLogic.getInstance().hasCustomerName(txtFieldName.getText())){
                 alertScreen("Cannot Update Account", "Please enter a valid account name", "", "Try Again");
-            } else if(DataLogic.getInstance().setIndividualAddress(txtFieldName.getText(), txtFieldStreetAddress1.getText(), txtFieldStreetAddress2.getText(),
-                    txtFieldCity.getText(), txtFieldState.getText(), txtFieldZip.getText())){
+            } else if(DataLogic.getInstance().setIndividualCustomer(txtFieldName.getText(), txtFieldStreetAddress1.getText(), txtFieldStreetAddress2.getText(),
+                    txtFieldCity.getText(), txtFieldState.getText(), txtFieldZip.getText(), txtFieldCardNumber.getText(), txtFieldCVV.getText(),
+                    expirationMonthOptions.getValue(), expirationYearOptions.getValue())){
                 alertScreen("Individual Account Update", "Account has been updated successfully", "", "Thank You");
                 switchScreen(event, "home.fxml");
             }else{
